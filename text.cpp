4dd985a25f17601bb25c5d70b516d39bcfb0bc07
Liam Sharp <lms464@scarletmail.rutgers.edu>
2021-10-14 12:31:15 -0400

    init

---
+++ text.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sstream>
+
+void text_out (std::string arr) {
+
+	for (int i = 0 ; i < arr.length(); i++) {
+		std::cout << arr[i] << std::flush;
+		usleep(70000);
+	}
+	std::cout << std::endl;
+	usleep(100000);
+}
+
+void Start_Accademica() {
+	std::string name;
+	std::string program;
+	std::string grad;
+	std::string S = "Hello World!\nWelcome to Ph.D. simulator, a text based grad school simulator!\nWhat is your name?";
+	text_out(S);
+	std::cout<<std::endl;
+	std::cin >> name;
+	std::ostringstream oss;
+	oss << "Welcome "<<name<<"!";
+	S = oss.str();
+	text_out(S);
+	std::cout<<std::endl;
+	S = "What school or program are you in?";
+	text_out(S);
+	std::cin >> program;
+	std::cout<<std::endl;
+	S = "What what year to expect to graduate?";
+	text_out(S);
+	std::cin >> grad;
+	std::cout<<std::endl;
+	S = "I'm not sure about that year... lets say you've got about 7 years from now.";
+	text_out(S);
+}
+
+int main () {
+
+/*/*	std::string out[] = {"Hello","world","!"};
+*//*	int str_len = sizeof(out)/sizeof(out[0]);*/
+	Start_Accademica();
+	return 0;
+}
